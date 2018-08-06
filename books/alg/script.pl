42521923d9eec0a21b7e1561e36673432ff0d673
Орлов Ярослав <orlov.avis@yandex.ru>
2018-08-06 23:37:44 +0300

    commit from script

--- books/alg/script.pl
+++ books/alg/script.pl
@@ -9,11 +9,20 @@ use Test::More;
 use MySearch;
 use MySort;
 
-my @origin_array = (12, 9, 3, 7, 14, 11);
+my @origin_array = (12, 9, 7, 7, 10, 5, 7);
 my @shuffle_array = @origin_array; # shuffle @origin_array;
 
 my $length = scalar(@shuffle_array);
-my @sort_array = MySort::merge_sort(\@shuffle_array, 0, $length - 1);
+# my @sort_array = MySort::merge_sort(\@shuffle_array, 0, $length - 1);
+my @sort_array = MySort::merge_sort(@shuffle_array);
+
+warn "\n\n";
+warn "result = @sort_array";
+
+
+__DATA__
+int[] a1 = new int[] {21, 23, 24, 40, 75, 76, 78, 77, 900, 2100, 2200, 2300, 2400, 2500};
+int[] a2 = new int[] {10, 11, 41, 50, 65, 86, 98, 101, 190, 1100, 1200, 3000, 5000};
 
 =head
 my $check = 1;
