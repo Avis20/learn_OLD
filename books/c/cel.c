e65b77b58710953bb7dc1bef553ba9a6f7c3155e
Орлов Ярослав <orlov.avis@yandex.ru>
2018-11-04 00:17:59 +0300

    commit from script

---
+++ books/c/cel.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Вывод таблицы температур Цельсию по Фаренгейту
+
+int main (){
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      // нижняя граница температур
+    upper = 300;    // верняя граница температур
+    step = 20;      // величина шага
+
+    while ( fahr <= upper ){
+        celsius = (5.0/9.0) * (fahr - 32.0); // c = (5/9)(f - 32)
+        printf("%3.0f %6.2f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}
