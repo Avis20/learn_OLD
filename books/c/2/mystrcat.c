e320521fea122c7f2a855abe92cb9b7830825edf
Орлов Ярослав <orlov.avis@yandex.ru>
2019-03-18 01:50:45 +0300

    commit from script

--- books/c/2/mystrcat.c
+++ books/c/2/mystrcat.c
@@ -4,8 +4,8 @@
 void mystrcat ( char s[], char t[]);
 
 int main(){
-    char str1[] = "hello, ";
-    char str2[] = "world!";
+    char str1[10] = "hello, ";
+    char str2[10] = "world!";
     mystrcat(str1, str2);
     printf("\n%s\n", str1);
     return 0;
@@ -15,5 +15,9 @@ void mystrcat ( char s[], char t[]){
     int i, j;
     i = j = 0;
     while ( s[i] != '\0' ) i++;
-    while ( ( s[i++] = t[j++] ) != '\0' );
+    printf("%d", i);
+    s[8] = 'd';
+    s[9] = 'd';
+    s[10] = 'd';
+    // while ( ( s[i++] = t[j++] ) != '\0' );
 }
--- books/c/3/switch.c
+++ books/c/3/switch.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+// Посчет кол-ва цифр, пробелов, других символов
+
 int main(){
     int c, i, nwhite, other, digits[10];
+
     nwhite = other = 0;
     for ( i = 0; i < 10; i++ ){
         digits[i] = 0;
         while ( ( c = getchar() ) != EOF ){
-        // while ( 1 ){
             switch(c){
                 case '0': case '1': case '2': case '3': case '4':
                 case '5': case '6': case '7': case '8': case '9':
