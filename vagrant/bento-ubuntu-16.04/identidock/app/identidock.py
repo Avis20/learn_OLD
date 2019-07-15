4ad6ce1a8cacf83a233adaefa8acbc6470739347
Орлов Ярослав <orlov.avis@yandex.ru>
2019-07-15 04:21:13 +0300

    commit from script Пн июл 15 04:21:13 MSK 2019

--- vagrant/bento-ubuntu-16.04/identidock/app/identidock.py
+++ vagrant/bento-ubuntu-16.04/identidock/app/identidock.py
@@ -1,9 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
+default_name = 'Avis'
+
 @app.route('/')
-def hello_world():
-    return "Hello, World!\n"
+def get_identicon():
+    name = default_name
+    header = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <title>Identidock</title> </head><body>'
+    body = '''
+    <from method="POST">
+        Hello <input type="text" name="name" value="{}"/>
+        <input type="submit" value="submit"/>
+    </from>
+    <p>You look like a: <img src="/monster/monstrer.png"/></p>
+    '''.format(name)
+    footer = '</body></html>'
+
+    return header + body + footer
+
+# def hello_world():
+#     return "Hello, World!\n"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
