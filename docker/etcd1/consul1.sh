c06821278fdd8d0789f8c3e71c0d65a4a298f080
Орлов Ярослав <y.orlov@fonmix.ru>
2019-09-24 00:05:11 +0300

    'master: commit from script Вт сен 24 00:05:11 MSK 2019'

--- docker/etcd1/consul1.sh
+++ docker/etcd1/consul1.sh
@@ -3,10 +3,10 @@
 HOSTA=3.3.3.2
 
 docker run --rm -d --name consul -h consul-1 \
--p 8300:8300 -p 8301:8301 -p 8301:8301/udp \
--p 8302:8302 -p 8400:8400 -p 8500:8500 -p $HOSTA:53:8600/udp \
-gliderlabs/consul agent -data-dir /data -server \
--client 0.0.0.0 -advertise $HOSTA -bootstrap-expect 2
+    -p 8300:8300 -p 8301:8301 -p 8301:8301/udp \
+    -p 8302:8302 -p 8400:8400 -p 8500:8500 -p $HOSTA:53:8600/udp \
+    gliderlabs/consul agent -data-dir /data -server \
+    -client 0.0.0.0 -advertise $HOSTA -bootstrap-expect 2
 
 exit;
 
